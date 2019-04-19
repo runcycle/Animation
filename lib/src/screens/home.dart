c282d7b4989995301ccd8b6a9b7a1f47b2b4ed88
runcycle <justinbartholomew@gmail.com>
2019-04-19 15:07:03 -0400

    Added cat image and created controls for up and down movement

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -1,20 +1,60 @@
 import 'package:flutter/material.dart';
+import '../widgets/cat.dart';
 
 class Home extends StatefulWidget {
   createState() => HomeState();
 }
 
-class HomeState extends State<Home> {
+class HomeState extends State<Home> with TickerProviderStateMixin {
+  Animation<double> catAnimation;
+  AnimationController catController;
+
+  initState() {
+    super.initState();
+
+    catController = AnimationController(
+      duration: Duration(seconds: 2),
+      vsync: this,
+    );
+    catAnimation = Tween(begin: 0.0, end: 100.0)
+      .animate(
+        CurvedAnimation(
+          parent: catController,
+          curve: Curves.easeIn,
+      ),
+    );
+  }
+
+  onTap() {
+    if (catController.status == AnimationStatus.completed) {
+      catController.reverse();
+    } else if (catController.status == AnimationStatus.dismissed) {
+      catController.forward();
+    }
+  }
+  
   Widget build(context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Animation!'),
+        title: Text('Animation'),
       ),
-      body: buildAnimation(),
+      body: GestureDetector (
+        child: buildAnimation(),
+        onTap: onTap,
+      )
     );
   }
 
   Widget buildAnimation() {
-    
+    return AnimatedBuilder(
+      animation: catAnimation,
+      builder: (context, child) {
+        return Container(
+          child: child,
+          margin: EdgeInsets.only(top: catAnimation.value),
+        );
+      },
+      child: Cat(),
+    );
   }
 }
